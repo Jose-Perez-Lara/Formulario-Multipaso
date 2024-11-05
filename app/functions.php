bad5df2630fde9b70d913ac3897e27cb44736f2a
usuario <jperlar802@g.educaand.es>
2024-11-05 14:53:44 +0100

    forma

--- app/functions.php
+++ app/functions.php
@@ -1,27 +1,94 @@
 <?php
-    function getFormMarkup($paso){
-        $formBody = "";
-        switch ($paso) {
-            case 1:
-                $formBody .= "<label>Genero: </label>";
-                $formBody .= "<select name='genero'>";
-                foreach ($_SESSION['form']['pasos'][1]['options'] as $opcion) {
-                    $formBody .= "<option value='$opcion'>$opcion</option>";
-                }
-                $formBody .= "</select>";
-                break;
-            case 2:
-                $formBody .= "<label>Musculos: </label>";
-                foreach ($_SESSION['form']['pasos'][2]['options'] as $opcion) {
+function getFormMarkup($paso)
+{
+    $formBody = "";
+    switch ($paso) {
+        case 1:
+            $formBody .= "<label>Genero: </label>";
+            $formBody .= "<select name='genero'>";
+            foreach ($_SESSION['form']['pasos'][1]['options'] as $opcion) {
+                $formBody .= "<option value='$opcion'>$opcion</option>";
+            }
+            $formBody .= "</select>";
+
+            $formBody .= '<button type="submit" name="accion" value="siguiente">Siguiente</button>';
+            break;
+        case 2:
+            $formBody .= "<label>Musculos: </label>";
+            foreach ($_SESSION['form']['pasos'][2]['options'] as $opcion) {
+
+                if (in_array($opcion, $_SESSION['form']['pasos'][2]['values'])) {
+                    $formBody .= "<input type='checkbox' checked name='musculos[]'value='$opcion'> $opcion <br>";
+                } else {
                     $formBody .= "<input type='checkbox' name='musculos[]'value='$opcion'> $opcion <br>";
                 }
-                break;
-            
-            default:
-                $formBody = "<h1>Paso no valido</h1>";
-                break;
-        }
+            }
+            $formBody .= '
+                <button type="submit" name="accion" value="atras">Atrás</button>
+                <button type="submit" name="accion" value="siguiente">Siguiente</button>';
+            break;
+        case 3:
+            foreach ($_SESSION['form']['pasos'][2]['values'] as $value) {
+                $formBody .= "<label>Peso(" . $value . "): </label>";
+                $formBody .= "<input type='text' name='pesos[" . $value . "]'> <br>";
 
-        return $formBody;
+                $formBody .= "<label>Repeticiones(" . $value . "): </label>";
+                $formBody .= "<input type='text' name='repeticiones[" . $value . "]'> <br>";
+            }
+
+
+            $formBody .= '
+                <button type="submit" name="accion" value="atras">Atrás</button>
+                <button type="submit" name="accion" value="siguiente">Siguiente</button>';
+            break;
+        case 4:
+
+            foreach ($_SESSION['form']['pasos'][2]['values'] as $musculo) {
+                $formBody .= "<ul>";
+
+                $peso = $_SESSION['form']['pasos'][3]['values']['peso_' . $musculo];
+                $repeticiones = $_SESSION['form']['pasos'][3]['values']['repeticiones_' . $musculo];
+
+                foreach ($_SESSION['form']['pasos'][4]['options'][$musculo] as $index => $value) {
+                    $formBody .= "<li>
+                    <label for='plan_." . $index . ">Plan para " . $musculo . " " . $index . "</label>
+                    <input type='radio' name='plan_" . $musculo . " id='plan_" . $index . "' value='plan_" . $index . "'>";
+
+                    $planes = preg_split("\n", $value);
+                    foreach ($planes as $plan) {
+                        $formBody .= "<ul>" . $plan . " con " . $peso . " kg a " . $repeticiones . " repeticiones</ul>";
+                    }
+
+
+                    $formBody .= "</li>";
+                }
+
+                $formBody .= "</ul>";
+            }
+            $formBody .= '
+                <button type="submit" name="accion" value="atras">Atrás</button>
+                <button type="submit" name="accion" value="siguiente">Siguiente</button>';
+            break;
+        case 5:
+
+            $formBody .= '
+                <button type="submit" name="accion" value="atras">Atrás</button>
+                <button type="submit" name="accion" value="finalizar">Finalizar</button>';
+            break;
+
+        default:
+            $formBody = "<h1>Paso no valido</h1>";
+            break;
     }
-?>
+
+    return $formBody;
+}
+
+
+function joinPesoYRepes($pesos, $repes)
+{
+    $finalArray = array();
+    foreach ($pesos as $key => $value) {
+        $tempArray = ["peso_" . $value, "repeticiones_" . $repes[$key]];
+    }
+}
--- index.php
+++ index.php
@@ -1,63 +1,100 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    session_start();
+session_start();
 
-    include_once('app/functions.php');
+include_once('app/functions.php');
 
-    if(!isset($_SESSION['form']) || empty($_SESSION['form'])){
-        $_SESSION['form'] = [
-            "pasoActual" => 1,
-            "pasos" => [
-                1=>[
-                    "options" =>["hombre","mujer"],
-                    "values" =>""
+if (!isset($_SESSION['form']) || empty($_SESSION['form'])) {
+    $_SESSION['form'] = [
+        "pasoActual" => 1,
+        "pasos" => [
+            1 => [
+                "options" => ["hombre", "mujer"],
+                "values" => []
+            ],
+            2 => [
+                "options" => ["pectoral", "biceps", "gluteos"],
+                "values" => []
+            ],
+            3 => [
+                "options" => [],
+                "values" => []
+            ],
+            4 => [
+                "options" => [
+                    "pectoral" => [
+                        "Press de banca con barra \nPress inclinado con mancuernas \nAperturas en máquina ",
+                        "Press declinado con barra \nFondos en paralelas \nAperturas en banco inclinado ",
+                        "Press plano con mancuernas \nAperturas en banco plano \nPress en máquina "
+                    ],
+                    "biceps" => [
+                        "Curl con barra \nCurl alterno con mancuernas \nCurl martillo ",
+                        "Curl predicador con barra \nCurl concentrado \nCurl con polea baja ",
+                        "Curl en banco inclinado \nCurl con mancuernas de pie \nCurl en polea alta "
+                    ],
+                    "gluteos" => [
+                        "Sentadillas profundas \nHip thrust con barra \nPatada de glúteo en máquina ",
+                        "Peso muerto sumo \nAbducción de cadera en máquina \nPuente de glúteos con barra ",
+                        "Elevación de cadera con peso \nSentadilla búlgara \nPatada trasera con polea "
+                    ]
                 ],
-                2=>[
-                    "options" =>["pectoral","biceps","gluteos"],
-                    "values" =>""
-                ],
-                3=>[
-                    "options" =>[],
-                    "values" =>""
-                ],
-                4=>[
-                    "options" => ["nombre", "email", "foto"],
-                    "values" =>[],
-                ],
-                5=>[
-                    "control" => "mostrar"
-                ]
+                "values" => []
+            ],
+            5 => [
+                "options" => ["nombre", "email", "foto"],
+                "values" => [],
             ]
-        ];
+        ]
+    ];
+}
 
+if (isset($_POST) && !empty($_POST)) {
+    $pasoActual = $_SESSION['form']['pasoActual'];
+    switch ($pasoActual) {
+        case 1:
+            $_SESSION['form']['pasos'][$pasoActual]['values'] = $_POST['genero'];
+            break;
+        case 2:
+            $_SESSION['form']['pasos'][$pasoActual]['values'] = $_POST['musculos'];
+            break;
+        case 3:
+            $pesos = $_POST['pesos'];
+            $repeticiones = $_POST['repeticiones'];
+            $pesosYRepeticiones = joinPesoYRepes($pesos, $repeticiones);
+            // array_push($_SESSION['form']['pasos'][$pasoActual]['values'])
+            //  = $_POST[];
+            break;
+
+        default:
+            # code...
+            break;
     }
+}
 
-    if(isset($_POST['accion']) && !empty($_POST['accion'])){
-        echo "ZZZZZZ";
-        switch ($_POST['accion']) {
-            case 'atras':
-                $_SESSION['form']['pasoActual'] = $_SESSION['form']['pasoActual'] - 1;
-                echo "AAAAA";
-                break;
 
-            case 'siguiente':
-                $_SESSION['form']['pasoActual'] = $_SESSION['form']['pasoActual'] + 1;
-                echo "BBBB";
-                break;
-            
-            default:
+
+if (isset($_POST['accion']) && !empty($_POST['accion'])) {
+
+    switch ($_POST['accion']) {
+        case 'atras':
+            $_SESSION['form']['pasoActual'] = $_SESSION['form']['pasoActual'] - 1;
+            break;
+
+        case 'siguiente':
+            $_SESSION['form']['pasoActual'] = $_SESSION['form']['pasoActual'] + 1;
+            break;
+
+        default:
             echo "CCCC";
-                break;
-        }
+            break;
     }
-    var_dump($_SESSION['form']);
-    
-
-    $formBody = getFormMarkup($_SESSION['form']['pasoActual']);
+}
 
 
-    include_once('templates/templateIndex.php');
-?>
+$formBody = getFormMarkup($_SESSION['form']['pasoActual']);
+
+
+include_once('templates/templateIndex.php');
